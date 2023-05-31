f053bb420c2826180b6bcbabadbe768c639f4d21
Saher <saherdev4@gmail.com>
2023-05-31 23:12:26 +0300

    using  lodash

--- packages/utilities/common-helpers/src/providers/object-fields-accessor.service.ts
+++ packages/utilities/common-helpers/src/providers/object-fields-accessor.service.ts
@@ -1,47 +1,14 @@
-const keySeparator = '.';
+import _ from 'lodash';
 export class ObjectFieldsAccessor {
   static setValue(
     targetKey: Readonly<string>,
     target: any = {},
     value: any
   ): void {
-    const keyParts = targetKey.split(keySeparator);
-    let currentTarget = target;
-
-    for (let i = 0; i < keyParts.length; i++) {
-      const keyPart = keyParts[i];
-
-      if (i === keyParts.length - 1) {
-        currentTarget[keyPart] = value;
-      } else {
-        if (isNaN(parseInt(keyParts[i + 1]))) {
-          if (!(keyPart in currentTarget)) {
-            currentTarget[keyPart] = {};
-          }
-        } else {
-          if (!(keyPart in currentTarget)) {
-            currentTarget[keyPart] = [];
-          }
-        }
-
-        currentTarget = currentTarget[keyPart];
-      }
-    }
+    _.set(target, targetKey, value);
   }
 
   static getValues(object: any = {}, accessKey: Readonly<string[]>): any[] {
-    return accessKey.map((key) => {
-      return key.split(keySeparator).reduce((currentValue, keyPart) => {
-        if (
-          currentValue &&
-          typeof currentValue === 'object' &&
-          keyPart in currentValue
-        ) {
-          return currentValue[keyPart];
-        } else {
-          return undefined;
-        }
-      }, object);
-    });
+    return accessKey.map((key) => _.get(object, key, undefined));
   }
 }

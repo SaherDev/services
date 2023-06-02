12493b2eeec04fe732d91f508a084c750734f7c3
Saher <saherdev4@gmail.com>
2023-06-02 14:49:30 +0300

    json processor

--- packages/utilities/adapter/src/data-processor/json-processor.ts
+++ packages/utilities/adapter/src/data-processor/json-processor.ts
@@ -1,11 +1,17 @@
 import { IAdapterParserConfig, IBufferProcessor } from '@services/models';
 
+import { ObjectFieldsAccessor } from '@services/common-helpers';
+
 export class JsonProcessor implements IBufferProcessor {
   async *toRowsAsync(
     buffer: any,
     config: IAdapterParserConfig
   ): AsyncGenerator<any, void, void> {
-    for (const x of buffer) {
+    const data = config?.dataPath
+      ? ObjectFieldsAccessor.getValues(buffer, [config?.dataPath])
+      : buffer;
+
+    for (const x of data) {
       yield x;
     }
   }

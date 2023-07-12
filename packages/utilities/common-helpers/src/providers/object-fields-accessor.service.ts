4e05761664925d43b42699ce2130cb762e21c090
Saher <saherdev4@gmail.com>
2023-07-12 20:41:45 +0300

    fillEmptyProperties function

--- packages/utilities/common-helpers/src/providers/object-fields-accessor.service.ts
+++ packages/utilities/common-helpers/src/providers/object-fields-accessor.service.ts
@@ -1,5 +1,25 @@
 import _ from 'lodash';
 export class ObjectFieldsAccessor {
+  static fillEmptyProperties<T>(obj: Partial<T>, properties: T): any {
+    if (typeof obj !== 'object' || obj === null || Array.isArray(obj))
+      return obj;
+
+    for (const key of Object.keys(properties)) {
+      if (!obj.hasOwnProperty(key)) {
+        obj[key] = properties[key];
+      } else if (
+        typeof obj[key] === 'object' &&
+        obj[key] !== null &&
+        typeof properties[key] === 'object' &&
+        properties[key] !== null
+      ) {
+        this.fillEmptyProperties(obj[key], properties[key]);
+      }
+    }
+
+    return obj;
+  }
+
   static setValue(
     targetKey: Readonly<string>,
     target: any = {},
